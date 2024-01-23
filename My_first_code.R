80eaba547af84528b6abe91916e57ed3402d62a8
zebenaybitew <151753915+zebenaybitew@users.noreply.github.com>
2024-01-23 15:52:09 +0100

    I added a lot of code lines

--- My_first_code.R
+++ My_first_code.R
@@ -13,3 +13,34 @@ weight_kg<-57
 2.2*weight_kg
 
 weight_lb<-2.2*weight_kg
+# I have to update the value of the weight_kg object 
+weight_kg<-100
+
+mass<-47.5
+age<-122
+mass<-mass*2.0
+age<-age-20
+mass_index<-mass/age
+
+####### functions 
+sqrt(9)
+?round()
+
+round(3.14159)
+round(3.14159, digits = 2)
+round(3.14159, 2)
+round(digits = 2, x=3.14159)
+round(2, 3.14159)# it will automatically choose 2
+round(digits = 2)# the object is missed so it won't work
+args(round)
+
+############ writing our own function 
+my_function <-function(x) {
+  x<-x+2
+}
+
+my_result<-my_function(3)
+
+
+
+
