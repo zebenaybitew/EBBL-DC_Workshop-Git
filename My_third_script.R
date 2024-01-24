90866b5a2891a2161f078526906b2b0b1417fbb1
zebenaybitew <151753915+zebenaybitew@users.noreply.github.com>
2024-01-24 10:07:36 +0100

    Fourth commit

--- My_third_script.R
+++ My_third_script.R
@@ -7,7 +7,10 @@ download.file(url="https://ndownloader.figshare.com/files/2292169",
 
 ####### read data in R 
 library(tidyverse)# this library is used to read this large data in R
+
+### read the data using read_csv command 
 surveys<- read_csv("data_raw/portal_data_joined.csv")
+
 read_csv() # is used to read data in more clear format
 read.csv() # is not robust as comared to read_csv
 
@@ -54,5 +57,54 @@ my_list<-list(names=c("lora", "Lisanna", "Francesco"),
               money=c(1,6,7,3,5,8))
 my_list[[1]]
 my_list$names
+
+
+### changing variables to factors  : useful to handle data frames and it only contains the pre-defined levels
+str(surveys)
+surveys$sex<-factor(surveys$sex) #it will automatically level female 1 and male 2 sequentially 
+levels(surveys$sex)
+nlevels(surveys$sex)# will provide us the number of levels 
+
+sex<-factor(c("male", "female", "female", "male"))
+sex<-factor(sex, levels = c("male", "female"))
+
+######## challenge chamge taxa and genus to a factor 
+surveys$taxa<- factor(surveys$taxa)
+levels(surveys$taxa)
+sum(surveys$taxa=="Rabbit") #to know the number of rabbits in taxa
+summary(surveys$taxa)
+
+surveys$genus<- factor(surveys$genus)
+nlevels(surveys$genus)# 26 genus
+
+######## convert factors
+as.character(sex)
+year_fct<-factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct) # 
+# we can always change numeric to character with out losing the data 
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct)) [year_fct] # the squre bracket provides the levels for each factor
+
+########## remaining factors 
+plot(surveys$sex)
+summary(surveys$sex)
+sex<-surveys$sex
+levels(sex)
+sex<-addNA(sex)# to add NA as a level
+levels(sex)
+
+levels(sex) [3]<-"undetermined" # is used to substitute NA to Undetermined 
+
+levels(sex)
+plot(sex)
+
+####### challenge 
+
+levels(sex) [1:2]<-c("female","male")
+plot(sex)
+
+sex<-factor(sex, levels=c("undetermined","female" , "male"))
+
+plot(sex)
 
 
