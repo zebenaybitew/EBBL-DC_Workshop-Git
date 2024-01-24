2458abef48a8bd872c55c901673b29cb734ba108
zebenaybitew <151753915+zebenaybitew@users.noreply.github.com>
2024-01-24 18:25:18 +0100

    ggplot_latest

--- my_R_code_ggplot.R
+++ my_R_code_ggplot.R
@@ -85,15 +85,108 @@ ggplot(data = surveys_complete, mapping = aes(x=species_id, y=hindfoot_length)) 
   geom_jitter(alpha=0.3, aes(color= factor(plot_id))) + # the factor function is added coz it considres plot_id as continous variable 
   geom_boxplot (outlier.shape = NA)
 
-
 ######## writing a line function nusing a time series data 
 yearly_count<-surveys_complete %>% 
   count(year, genus)
 ###### a line function nfor a time series data 
-ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus )) +
-  geom_line()
+ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus, group=genus )) +
+  geom_line() 
+  
+######## ggplot can be integrated to the tidyverese package or function will provide the same plot
 
+yearly_count %>% 
+  ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus)) +
+  geom_line() 
 
+#######  the same graph combining ggplot with tidyverse
+yearly_count_graph<-yearly_count %>% 
+  count(year, genus) %>% 
+  ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus)) +
+  geom_line() 
+yearly_count_graph
+
+ ####### a matrix of plots using faceting  gives separate graphs 
+ggplot(data=yearly_count, mapping = aes(x=year, y=n)) +
+  geom_line() +
+  facet_wrap(facets = vars(genus))
+  
+######## drawing data classified based on gender 
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_wrap(facets = vars(genus))
+
+####### 
+
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_grid(rows = vars(sex), cols = vars(genus))
+
+########
+
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_grid(rows = vars(genus))
+
+#########
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_grid(rows = vars(genus))
+
+########
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_wrap(facets = vars(genus)) +
+  theme_bw() # improves the resolution of functions 
+
+######## to change the base size 
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_wrap(facets = vars(genus)) +
+  theme_bw(base_size = 18)
+#########
+plt<-surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(
+    mapping = aes(
+      x=year, 
+      y=n, 
+      color=sex)) +
+  geom_line() +
+  facet_wrap(facets = vars(genus)) +
+  scale_color_manual(
+    values = c("tomato", "dodgerblue"),
+    labels= c("female", "male"),
+    name="Sex") +
+  xlab("year of observation") +
+  ylab("number of individulas") +
+  ggtitle("Observed genera over the time")
+  theme_bw(base_size = 14) +
+    theme(
+      legend.position="bottom", 
+      aspect.ratio=1, 
+      axis.text.x=element_text(
+        angle=45, 
+        hjust=1
+      ),
+      plot.title = element_text(hjust = 0.5),
+      panel.grid=element_blank(), 
+      strip.background = element_blank()
+         )
+ plt
+####### 
+ggsave(filename = "data/plot.pdf", plot = plt, width = 20, height = 20)
 
 
 
