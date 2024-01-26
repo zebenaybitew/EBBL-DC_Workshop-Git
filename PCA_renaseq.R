88cea3996e6cbcb2c356a0b106f9bd4848ffde96
zebenaybitew <151753915+zebenaybitew@users.noreply.github.com>
2024-01-26 12:50:53 +0100

    upddate on PCA and MA anlayses

--- PCA_renaseq.R
+++ PCA_renaseq.R
@@ -124,6 +124,131 @@ tidy(sample_pca, matrix="eigenvalues")
 
 tidy (sample_pca, matrix="loadings")
 
+#########
+autoplot(sample_pca, 
+         data = sample_info %>% mutate(minute=as.factor(minute)), 
+                                       colour="minute",
+                                       shape="strain")
+
+         
+test_result<-read_csv("data_rnaseq/test_result.csv")  
+
+#### differential expression results 
+# gene column-> gene name
+# basMean column-> normalized expression of a gene
+#log2Foldachnage column-> amount of change between two conditions 
+# lfcSE column-> standard error associated to log2FoldCahnge value 
+# star column -> statistics value compurted as log2FoldChange/lfcSE compared to the stamndard normal distribution
+# pvalue-> p-value associated with the change 
+# padj-> p-value corrected for multiple hypothesis testing
+# comparison-> comparison group
+# genes with hihger basmean value tend to change less 
+
+
+
+######### MA plot
+# generate MA plot (baseMean vs log2FoldChange and compare by time point; Hint: consider log-transform baseMean)
+test_result %>% 
+ggplot(aes(x = log10(baseMean) , y =log2FoldChange )) +
+  geom_point(alpha=0.1) +
+  facet_wrap(facets = vars(comparison)) 
+
+######## 
+test_result %>% 
+  mutate(sig=ifelse(padj<0.01, log2FoldChange, NA))  %>% 
+  ggplot(aes(x = log10(baseMean) , y =log2FoldChange )) +
+  geom_point(alpha=0.1) +
+  geom_point(aes(y=sig), color="tomato", size=1) +
+  geom_hline(yintercept = 0, color="dodgerblue")+
+  facet_wrap(facets = vars(comparison)) 
+  
+
+ma_plot<-test_result %>% 
+  mutate(sig=ifelse(padj<0.01, log2FoldChange, NA))  %>% 
+  ggplot(aes(x = log10(baseMean) , y =log2FoldChange )) +
+  geom_point(alpha=0.1) +
+  geom_point(aes(y=sig), color="tomato", size=1) +
+  geom_hline(yintercept = 0, color="dodgerblue")+
+  facet_wrap(facets = vars(comparison)) 
+
+### combining two plots 
+(ma_plot|pca_plot)
+
+##### genes significantly associaeted or candidate genes 
+
+
+# visualizing expresssion trends 
+# step 1: to get candidate genes (aka padj<0.01)
+candidate_gene<-test_result %>% 
+  filter(padj<0.01) %>% 
+  pull(gene) %>% # test_results [, "gene] aka test_result$gene: extraxts one column in to a vector 
+  unique()
+# 1a. get the trans_cts_long
+trans_cts_long<-trans_cts %>% 
+  pivot_longer(cols = wt_0_r1:mut_180_r3, names_to = "sample",
+               values_to = "cts") %>% 
+  full_join(sample_info, by="sample")
+
+# 2. filter trans_cts_long for candidate genes and compute mean expression value for each in each tp and genotype
+
+trans_cts_mean<-trans_cts_long %>% 
+  filter(gene%in%candidate_gene) %>% 
+  group_by(gene, strain, minute) %>% 
+  summarize(mean_cts=mean(cts), nrep=n( )) %>% 
+  ungroup()
+
+# 3. plot trends 
+trans_cts_mean %>% 
+  ggplot(aes(x=minute, y=mean_cts)) +
+  geom_line(aes(group=gene), alpha=0.3)+
+  facet_grid(rows = vars(strain))
+
+# scaling data to improve visualization: z-score transform and the mean of all genes will be zero
+trans_cts_mean<-trans_cts_long %>% 
+  filter(gene%in%candidate_gene) %>% 
+  group_by(gene) %>% 
+  mutate(cts_scaled=(cts-mean(cts))/sd(cts)) %>% 
+  group_by(gene, strain, minute) %>% 
+  summarize(mean_cts_scaled=mean(cts_scaled), 
+            nrep=n( )) %>% 
+  ungroup()
+
+#### plot the scald graph is used for defferential expression analysis 
+trans_cts_mean %>% 
+  ggplot(aes(x=minute, y=mean_cts_scaled)) +
+  geom_line(aes(group=gene), alpha=0.3)+
+  geom_hline(yintercept = 0, color="brown", linetype="dashed")+
+  facet_grid(rows = vars(strain)) +
+  scale_x_continuous (breaks=unique(trans_cts_mean$minute)) # used to chnage the baseline scales 
+
+
+
+######## clustering analysis (supervised and non supervised analysis)
+# clustering analysis has two main steps
+# 1. measuring geometric distance or correlation distance matrix
+# 2. 
+
+# clustering 
+trans_cts<-read_csv("data_rnaseq/counts_transformed.csv")
+# 1. crreate a matrix of counts 
+hclust_matrx<-trans_cts %>% 
+  select(-gene) %>% 
+  as.matrix()
+rownames(hclust_matrx) <-trans_cts$gene
+hclust_matrx<-hclust_matrx[candidate_gene,]
+
+dim(hclust_matrx)
+
+
+
+# z score transform and transpose it
+hclust_matrx<-hclust_matrx %>% 
+  t() %>% 
+  scale() %>% 
+  t()
+
+dim(hclust_matrx)
+# 2. calculate distance matrix for the candidate gene
 
 
 
@@ -132,6 +257,26 @@ tidy (sample_pca, matrix="loadings")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
