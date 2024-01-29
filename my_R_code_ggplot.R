795f3b62534c36bd3730d8a6930f1f1264c1f1b2
zebenaybitew <151753915+zebenaybitew@users.noreply.github.com>
2024-01-29 12:34:44 +0000

    ggplot2_latest version

--- my_R_code_ggplot.R
+++ my_R_code_ggplot.R
@@ -1,6 +1,7 @@
 ########### data visualization using ggplot2
 
 library(ggplot2)
+
 #### Assign the ggplot
 plt<-ggplot(
   data = surveys_complete,
@@ -16,8 +17,9 @@ plt<-ggplot(
  plt +
    geom_point() +
    ggtitle("My first plot")
- # geom_ stands for gemeotric function 
+ # geom_ stands for geometric function 
  
+ #### 
  plt<-ggplot(data = surveys_complete, mapping = aes(x=weight, y=hindfoot_length)) +
    geom_point() 
  plt +
@@ -26,21 +28,23 @@ plt<-ggplot(
 install.packages("hexbin")  # used for hexagonal heatmap of 2d bin counts 
 library(hexbin)
 
-###### it adds or counts the number of observation 
+#### it adds or counts the number of observation 
 ggplot(data = surveys_complete, mapping = aes(x=weight, y=hindfoot_length)) +
   geom_hex()
-####### alpha is used to check the trasparency of the plots 
+
+#### alpha is used to check the transparency of the plots 
 ggplot(data = surveys_complete, mapping = aes(x=weight, y=hindfoot_length)) +
   geom_point(alpha= 0.1) 
 
-######## change color 
+#### change color 
 ggplot(data = surveys_complete, mapping = aes(x=weight, y=hindfoot_length)) +
   geom_point(alpha= 0.1, color="blue") 
 
-####### change color by variable name
+#### change color by variable name
 ggplot(data = surveys_complete, mapping = aes(x=weight, y=hindfoot_length)) +
   geom_point(alpha= 0.25, aes(color=species_id)) 
-####### or in more clear way 
+
+#### or in more clear way 
 ggplot(
   data = surveys_complete,
   mapping = aes(
@@ -52,95 +56,114 @@ ggplot(
    geom_point(alpha= 0.25) 
 
 
-####### challenge§ scatter plot of weightvs speciesId and color by plot_type
-
+#### challenge§ scatter plot of weight vs species Id and color by plot_type
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
-  geom_point(alpha= 0.25, aes(color=plot_type)) 
+  geom_point(alpha= 0.99, aes(color=plot_type)) 
 
-#######  better to use box plot for the above plot 
+#### better to use box plot for the above plot 
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
   geom_boxplot() 
 
-####### addind a liitle vale for each x value using  jitter
+#### adding a title value for each x value using  geom_jitter()
+# geom_jitter() is used to create scatter plots with jittered points, helping to visualize the distribution and density of points, especially when there is overlap.
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
   geom_boxplot(outlier.shape = NA) +    # it removes the shapes of the outlines
   geom_jitter(alpha=0.3, color="salmon")
-######## 
+
+#### color by plot_type
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
-  geom_boxplot(outlier.shape = NA) +    # it removes the shapes of the outlines
+  geom_boxplot(outlier.shape = NA) +    
   geom_jitter(alpha=0.3, aes(color=plot_type))
-######### 
+
+####
+ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
+  geom_jitter(alpha=0.3, color="salmon") +
+  geom_boxplot(outlier.shape = NA, fill="NA")
+
+#### fill box with "green color"
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
     geom_jitter(alpha=0.3, color="salmon") +
-   geom_boxplot(outlier.shape = NA, fill=NA)
+   geom_boxplot(outlier.shape = NA, fill="green")
 
-########### produce a violin plot of weight by species_id
+#### produce a violin plot of weight by species_id
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
   geom_violin ()+
   scale_y_log10() +# used to make it more visible 
-  ylab("weight(log10)") 
+  ylab("weight(log10)") +
+  xlab("Species ID")
 
-###### challenge: make a box plot + jittered scatter plot of hindfoot_length by species_id: box plt should be in fromnt off the dot filled with white 
+#### violin filled with red color
+ggplot(data = surveys_complete, mapping = aes(x=species_id, y=weight)) +
+  geom_jitter(alpha=0.3, color="gray") +
+  geom_violin (fill="red")+
+  scale_y_log10() +
+  ylab("weight(log10)") +
+  xlab("Species ID")
+
+#### challenge: make a box plot + jitter scatter plot of hindfoot_length by species_id: box plot should be in front of the dot filled with white 
 ggplot(data = surveys_complete, mapping = aes(x=species_id, y=hindfoot_length)) +
-  geom_jitter(alpha=0.3, aes(color= factor(plot_id))) + # the factor function is added coz it considres plot_id as continous variable 
+  geom_jitter(alpha=0.3, aes(color= factor(plot_id))) + # the factor function is added coz it considers plot_id as continuous variable 
   geom_boxplot (outlier.shape = NA)
 
-######## writing a line function nusing a time series data 
+#### if we don't consider plot ID as a factor the plot will look different
+ggplot(data = surveys_complete, mapping = aes(x=species_id, y=hindfoot_length)) +
+  geom_jitter(alpha=0.3, aes(color= (plot_id))) + 
+  geom_boxplot (outlier.shape = NA)
+
+####  writing a line function using a time series data 
 yearly_count<-surveys_complete %>% 
   count(year, genus)
-###### a line function nfor a time series data 
-ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus, group=genus )) +
+
+#### plot a line function n for a time series data 
+ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus )) +
   geom_line() 
   
-######## ggplot can be integrated to the tidyverese package or function will provide the same plot
-
+#### ggplot can be integrated to the tidyverse package or function will provide the same plot
 yearly_count %>% 
   ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus)) +
   geom_line() 
 
-#######  the same graph combining ggplot with tidyverse
+#### the same graph combining ggplot with tidyverse
 yearly_count_graph<-yearly_count %>% 
   count(year, genus) %>% 
   ggplot(data=yearly_count, mapping = aes(x=year, y=n, color=genus)) +
   geom_line() 
 yearly_count_graph
 
- ####### a matrix of plots using faceting  gives separate graphs 
+#### a matrix of plots using faceting  gives separate graphs 
 ggplot(data=yearly_count, mapping = aes(x=year, y=n)) +
   geom_line() +
   facet_wrap(facets = vars(genus))
   
-######## drawing data classified based on gender 
+####  drawing data classified based on gender 
 surveys_complete %>% 
   count(year, genus, sex) %>% 
 ggplot(mapping = aes(x=year, y=n, color=sex)) +
   geom_line() +
   facet_wrap(facets = vars(genus))
 
-####### 
-
+#### used to separate graph by sex and genus 
 surveys_complete %>% 
   count(year, genus, sex) %>% 
   ggplot(mapping = aes(x=year, y=n, color=sex)) +
   geom_line() +
   facet_grid(rows = vars(sex), cols = vars(genus))
 
-########
+#### used to group graphs by sex in row 
+surveys_complete %>% 
+  count(year, genus, sex) %>% 
+  ggplot(mapping = aes(x=year, y=n, color=sex)) +
+  geom_line() +
+  facet_grid(rows = vars(sex))
 
+#### used to group graphs by genus in row 
 surveys_complete %>% 
   count(year, genus, sex) %>% 
   ggplot(mapping = aes(x=year, y=n, color=sex)) +
   geom_line() +
   facet_grid(rows = vars(genus))
 
-#########
-surveys_complete %>% 
-  count(year, genus, sex) %>% 
-  ggplot(mapping = aes(x=year, y=n, color=sex)) +
-  geom_line() +
-  facet_grid(rows = vars(genus))
-
-########
+#### theme_bw() used to have a graph with good resolution 
 surveys_complete %>% 
   count(year, genus, sex) %>% 
   ggplot(mapping = aes(x=year, y=n, color=sex)) +
@@ -148,14 +171,15 @@ surveys_complete %>%
   facet_wrap(facets = vars(genus)) +
   theme_bw() # improves the resolution of functions 
 
-######## to change the base size 
+#### to change the base size to 18
 surveys_complete %>% 
   count(year, genus, sex) %>% 
   ggplot(mapping = aes(x=year, y=n, color=sex)) +
   geom_line() +
   facet_wrap(facets = vars(genus)) +
   theme_bw(base_size = 18)
-########
+
+#### The final graph  of the ggplot 
 # Our last super complicated plot
  plt <- surveys_complete %>%
    count(year, genus, sex) %>%
@@ -186,7 +210,7 @@ surveys_complete %>%
    )
  plt
  
-  ####### 
+#### save the result
 ggsave(filename = "data/plot.pdf", plot = plt, width = 20, height = 20)
 
 
